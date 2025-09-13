18d8b016fc916cc53a0ba048f856e4411bd7977b
zeyad12-d <zeadyasser054@gmail.com>
2025-09-13 21:00:32 +0300

    Feat:CategoryDto

---
+++ AlAkelaSystem/DTO/CategoryDtos/CategoryDetailsDto.cs
@@ -0,0 +1,20 @@
+﻿using DTO.ProductDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO.CategoryDtos
+{
+    public class CategoryDetailsDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string Icon { get; set; } = null!;
+
+        public List<ProductResponesDto> Products { get; set; } = new List<ProductResponesDto>();
+    }
+}
--- AlAkelaSystem/DTO/obj/Debug/net8.0/DTO.AssemblyInfo.cs
+++ AlAkelaSystem/DTO/obj/Debug/net8.0/DTO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DTO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9365d2f66ba3a178521208edf7b902c13057a09")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c48d9193a9c615dc3da3badc72db9a4d917ac17a")]
 [assembly: System.Reflection.AssemblyProductAttribute("DTO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DTO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
