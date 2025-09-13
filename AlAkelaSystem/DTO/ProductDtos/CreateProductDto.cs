b9365d2f66ba3a178521208edf7b902c13057a09
zeyad12-d <zeadyasser054@gmail.com>
2025-09-13 20:53:20 +0300

    Feat:ProducDto

---
+++ AlAkelaSystem/DTO/ProductDtos/CreateProductDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO.ProductDtos
+{
+    public  class CreateProductDto
+    {
+        public string Name { get; set; } = null!;
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
---
+++ AlAkelaSystem/DTO/ProductDtos/ProductResponesDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO.ProductDtos
+{
+    public class ProductResponesDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+
+        public string Description { get; set; } = null!;
+
+        public decimal Price { get; set; }
+
+        public int Stock { get; set; }
+    }
+}
