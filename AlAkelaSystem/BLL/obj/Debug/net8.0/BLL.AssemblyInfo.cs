3ec1ec2aec84e0b2f1d8c1d3367f22e698d96a2d
zeyad12-d <zeadyasser054@gmail.com>
2025-09-26 12:55:33 +0300

    feat: Discount controller

--- AlAkelaSystem/BLL/obj/Debug/net8.0/BLL.AssemblyInfo.cs
+++ AlAkelaSystem/BLL/obj/Debug/net8.0/BLL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BLL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab392097cd95d353703a183a4596ebfaa0babf86")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+932e6c55bee51d056cd4f6b6689014b4322b57d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("BLL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BLL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ AlAkelaSystem/PL/Controllers/DiscountController.cs
@@ -0,0 +1,92 @@
+﻿using BLL.Interfaces.ManagerServices;
+using DTO.DiscountDtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PL.Controllers
+{
+    public class DiscountController : Controller
+    {
+        private readonly IServicesManager _servicesManager;
+        public DiscountController(IServicesManager servicesManager)
+        {
+            _servicesManager = servicesManager;
+        }
+        public async Task< IActionResult >Index()
+        {
+            var result = await _servicesManager.DiscountServices.GetAllDiscountsAsync();
+
+            return View(result.Data ?? new List<DiscountResponseDto>());
+        }
+        [HttpGet]
+        public async Task< IActionResult> Details( int id )
+        {
+            var result =  await _servicesManager.DiscountServices.GetDiscountByIdAsync(id);
+            if (result.StatusCode != 200 || result.Data == null)
+                return NotFound(result.Message);
+            return View(result.Data);
+
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateDiscountDto dto)
+        {
+            if (!ModelState.IsValid)
+                return View(dto);
+            var result = await _servicesManager.DiscountServices.CreateDiscountAsync(dto);
+            if (result.StatusCode == 200 || result.StatusCode == 201)
+                return RedirectToAction(nameof(Index));
+            ViewBag.Error = result.Message;
+            return View(dto);
+
+        }
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var result = await _servicesManager.DiscountServices.GetDiscountByIdAsync(id);
+            if (result.StatusCode != 200 || result.Data == null)
+                return NotFound(result.Message);
+            var dto = new UpdateDiscountDto
+            {
+                discountName = result.Data.discountName,
+                discountValue = result.Data.discountValue
+            };
+            return View(dto);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, UpdateDiscountDto dto)
+        {
+            if (!ModelState.IsValid)
+                return View(dto);
+            var result = await _servicesManager.DiscountServices.UpdateDiscountAsync(id, dto);
+            if (result.StatusCode == 200)
+                return RedirectToAction(nameof(Index));
+            ViewBag.Error = result.Message;
+            return View(dto);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var result = await _servicesManager.DiscountServices.GetDiscountByIdAsync(id);
+            if (result.StatusCode != 200 || result.Data == null)
+                return NotFound(result.Message);
+            return View(result.Data);
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var result = await _servicesManager.DiscountServices.DeleteDiscountAsync(id);
+            if (result.StatusCode == 200)
+                return RedirectToAction(nameof(Index));
+            ViewBag.Error = result.Message;
+            var discount = await _servicesManager.DiscountServices.GetDiscountByIdAsync(id);
+            return View(discount.Data);
+        }
+    }
+}
